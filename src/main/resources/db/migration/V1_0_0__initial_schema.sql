83c35e91189b28d88c9dfc8b29a04dbb5db7c9f5
Petar <petar.harambasa@racunarstvo.hr>
2023-04-03 15:49:30 +0200

    added docker files and postgresql settings

---
+++ src/main/resources/db/migration/V1_0_0__initial_schema.sql
@@ -0,0 +1,17 @@
+CREATE SCHEMA IF NOT EXISTS webShopSchema;
+
+CREATE TABLE if not exists user_role (
+                                         id_user_role SERIAL PRIMARY KEY ,
+                                         name varchar(255) not null
+    );
+
+CREATE TABLE if not exists shop_user (
+                                         id_shop_user SERIAL PRIMARY KEY ,
+                                         userName varchar(255) not null ,
+    email varchar(255) not null ,
+    password varchar(255) not null ,
+    user_role_id int not null REFERENCES user_role(id_user_role)
+    );
+
+INSERT INTO user_role (name) VALUES ('ADMIN');
+INSERT INTO user_role (name) VALUES ('CUSTOMER');
