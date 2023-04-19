17b9d24a00a7688bb7c32f5123b77cc6bc857b5e
PetarHarambasa <croman.3365@gmail.com>
2023-04-19 21:05:16 +0200

    made alter table scheme for database bug

--- src/main/resources/db/migration/V1_0_0__initial_schema.sql
+++ src/main/resources/db/migration/V1_0_0__initial_schema.sql
@@ -1,18 +1,18 @@
 CREATE SCHEMA IF NOT EXISTS web_shop_schema;
 
-CREATE TABLE if not exists user_role (
+CREATE TABLE if not exists web_shop_schema.user_role (
      id_user_role SERIAL PRIMARY KEY ,
      name varchar(255) not null
-    );
+);
 
-CREATE TABLE if not exists shop_user (
+CREATE TABLE if not exists web_shop_schema.shop_user (
     id_shop_user SERIAL PRIMARY KEY ,
     username varchar(255) not null ,
     email varchar(255) not null ,
     password varchar(255) not null ,
     authenticated boolean ,
-    user_role_id int not null REFERENCES user_role(id_user_role)
-    );
+    user_role_id int not null REFERENCES web_shop_schema.user_role(id_user_role)
+);
 
-INSERT INTO user_role (name) VALUES ('ADMIN');
-INSERT INTO user_role (name) VALUES ('CUSTOMER');
+INSERT INTO web_shop_schema.user_role (name) VALUES ('ADMIN');
+INSERT INTO web_shop_schema.user_role (name) VALUES ('CUSTOMER');
